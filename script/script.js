a0eb5410105ee7348555f9538271668b46080038
Denis Fedorov <ssboshen@yandex.ru>
2023-07-15 07:37:00 +0300

    refactor: change window-path search

--- script/script.js
+++ script/script.js
@@ -4,18 +4,18 @@ const fileName = ['cart',
   'product',
   'registration'];
 
-const currentPath = window.location.pathname;
+const currentPath = window.location.href;
 const pathParts = currentPath.split('/');
 pathParts.pop();
 const newPath = pathParts.join('/');
+console.log(newPath);
 const headerSearchInputEl = document.querySelector('.header__search-input');
 
 headerSearchInputEl.addEventListener('change', () => {
   const searchQuery = headerSearchInputEl.value;
   console.log(searchQuery);
   if (fileName.includes(searchQuery)) {
-    const protocol = window.location.protocol;
-      const url = `${protocol}//${newPath}/${searchQuery}.html`;
+    const url = `${newPath}/${searchQuery}.html`;
     window.location.href = url;
   }
 });
